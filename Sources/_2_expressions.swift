9cec7ba5b0f66b2817660996cfdda35238c031de
Alban Linard <alban@linard.fr>
2017-04-10 16:23:09 +0200

    Add semantics for + and -.

--- Sources/_2_expressions.swift
+++ Sources/_2_expressions.swift
@@ -1,1 +1,58 @@
 import LogicKit
+
+func constant (_ n : Int) -> Term {
+  return Value (n)
+}
+
+func plus (_ lhs : Term, _ rhs : Term) -> Map {
+  return [
+    "op"  : Value ("+"),
+    "lhs" : lhs,
+    "rhs" : rhs,
+  ]
+}
+
+func minus (_ lhs : Term, _ rhs : Term) -> Map {
+  return [
+    "op"  : Value ("-"),
+    "lhs" : lhs,
+    "rhs" : rhs,
+  ]
+}
+
+func eval (_ input: Term, _ output: Term) -> Goal {
+  func binary (op: @escaping (Term, Term) -> Term, semantics: @escaping (Term, Term) -> Term) -> Goal {
+    return freshn { g in
+      let l  = g ["l"]
+      let r  = g ["r"]
+      let lv = g ["lv"]
+      let rv = g ["rv"]
+      return input === op (l, r) &&
+             eval (l, lv) &&
+             eval (r, rv) &&
+             inEnvironment { s in
+               return output === semantics (s [lv], s [rv])
+             }
+    }
+  }
+  return
+    (isValue (input, Int.self) && input === output)
+    ||
+    binary (op: plus, semantics: { lhs, rhs in
+      switch (lhs, rhs) {
+      case let (lhs, rhs) as (Value<Int>, Value<Int>):
+        return Value (lhs.wrapped + rhs.wrapped)
+      default:
+        assert (false)
+      }
+    })
+    ||
+    binary (op: minus, semantics: { lhs, rhs in
+      switch (lhs, rhs) {
+      case let (lhs, rhs) as (Value<Int>, Value<Int>):
+        return Value (lhs.wrapped - rhs.wrapped)
+      default:
+        assert (false)
+      }
+    })
+}
--- Tests/02-expressionsTests/_2_expressionsTests.swift
+++ Tests/02-expressionsTests/_2_expressionsTests.swift
@@ -1,8 +1,16 @@
 import XCTest
 @testable import _2_expressions
 
+import LogicKit
+
 class _2_expressionsTests: XCTestCase {
     func testExample() {
+      let t = minus (constant (1), plus (constant (2), constant (3)))
+      let v = Variable (named: "v")
+      let goal = eval (t, v)
+      for r in solve (goal) {
+        print (r.reified () [v])
+      }
     }
 
     static var allTests = [
